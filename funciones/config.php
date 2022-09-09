d29e5ff1421808b6748c4330b20252aed1c15b17
nachojk <cuenta7hoi@gmail.com>
2022-09-09 13:43:53 -0500

    nuevos cambios

--- funciones/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-date_default_timezone_set("America/Mexico_City");
-
-define('DB_NAME', 'nombredetubasededatos');
-define('DB_USER', 'nombredeusuario');
-define('DB_PASSWORD', 'contraseña');
-define('DB_HOST', 'localhost');
-?>
